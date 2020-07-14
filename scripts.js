d60442006f840d6705a958a433f43a2c252d4c2d
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-14 13:23:21 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Som Nath");
+$("#policyNumber").text("RML1200714");
+$("#startDate").text("15-Jul-2020");
+$("#endDate").text("14-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DM1332");
+$("#make").text("TVS");
+$("#model").text("SPORT");
+$("#vehicalSubType").text("CVTI");
+$("#engineNo").text("CF5HG1293444");
+$("#chassis").text("MD625MF51G3H90899");
